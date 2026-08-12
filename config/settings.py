e01cab4d87564573aa14554eaa42aa935108c09c
Yasin <ybasiam360@gmail.com>
2026-08-12 06:40:06 +0600

    Adjusted throttle for easier checking

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,8 @@ REST_FRAMEWORK={
     ],
 
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '10/day',
+        # 'anon': '10/day', #NOTE: This was made for original code, next one is given for easier checking
+        'anon': '10/hour',
         'user': '50/day',
     }
 
